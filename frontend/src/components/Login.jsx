87b60ca307800dbdcc1fcef558ce7acb9852eb26
nguyenphucminh <59383987+nguyenphucminh@users.noreply.github.com>
2026-05-04 22:09:40 +0700

    tesst

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -72,7 +72,7 @@ function Login() {
                                         type="submit"
                                         onClick={handleSubmit}
                                         className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                                        Login
+                                        Login HERE
                                     </button>
                                     <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                                         Don't have an account?
